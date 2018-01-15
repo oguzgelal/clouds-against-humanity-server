2176f634deb749081eb5122ddab8472709aa249f
Oguz Gelal <oguz.gelal@vub.ac.be>
2018-01-15 16:12:54 +0100

    deploy redis server and config

--- src/config/redis-options.js
+++ src/config/redis-options.js
@@ -1,6 +1,4 @@
-module.exports = {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+const opts = {
   retry_strategy: options => {
     if (options.error && options.error.code === 'ECONNREFUSED') { return new Error('The server refused the connection'); }
     if (options.total_retry_time > 1000 * 60 * 60) { return new Error('Retry time exhausted'); }
@@ -8,3 +6,12 @@ module.exports = {
     return Math.min(options.attempt * 100, 3000);
   }
 }
+
+if (process.env.REDIS_URL) {
+  opts.url = process.env.REDIS_URL;
+} else {
+  opts.host = process.env.REDIS_HOST;
+  opts.port = process.env.REDIS_PORT;
+}
+
+module.exports = opts;
